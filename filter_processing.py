fa60bffa2f98f4a66bd8d94f85413fc9f8d3012c
nnathanyc <nathan31@illinois.edu>
2025-04-03 19:58:20 -0500

    Create filter_processing.py

---
+++ filter_processing.py
@@ -0,0 +1,4 @@
+#this is going to be the file where I will take elements fromt he desired image filter 
+#properties of this include exposure, hue, saturation, grain, brightness, color
+#To incorporate LUT, pillow module will be needed
+#goal for this file is to transfer contents from LUT files into py format
