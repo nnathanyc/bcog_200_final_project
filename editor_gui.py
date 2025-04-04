8fbe6e3a8f0c508e36e2e1e66042ee92051ef87b
nnathanyc <nathan31@illinois.edu>
2025-04-03 20:23:24 -0500

    Create editor_gui.py

---
+++ editor_gui.py
@@ -0,0 +1,5 @@
+#This will be the file for the image editor GUI
+#Contents for the GUI will include a home page, containing brief instructions 
+#will also have a button on the bottom that says "open image"
+#files that will be supported will be jpg and jpeg files from the user. 
+#will strive to make the UI as modern as possible, while keeping it minimalistic
