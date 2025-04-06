2271a2e84a4307a21934c85da8446cbd34cde64e
nnathanyc <nathan31@illinois.edu>
2025-04-06 15:16:19 -0500

    Update editor_gui.py

--- editor_gui.py
+++ editor_gui.py
@@ -1,5 +1,6 @@
 #This will be the file for the image editor GUI
 #Contents for the GUI will include a home page, containing brief instructions 
 #will also have a button on the bottom that says "open image"
+#after the process, there will be a button that gives the user an option to save the photo
 #files that will be supported will be jpg and jpeg files from the user. 
 #will strive to make the UI as modern as possible, while keeping it minimalistic
