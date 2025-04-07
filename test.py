d71a2708d243a201e47bc035f9d903af683fb76e
nnathanyc <nathan31@illinois.edu>
2025-04-07 15:09:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-
+#This will be my basic test file
+#will include simple steps in the logic of my program to test the output without running the entire program
+#First thing to consier is that the program provides an option for an input
+#Second thing to consider is that the program will provide an output with the applied LUT filter
