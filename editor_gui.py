4b155103ec542e2c3a67fe8481fc417b28922842
nnathanyc <nathan31@illinois.edu>
2025-04-18 13:35:56 -0500

    Update editor_gui.py

--- editor_gui.py
+++ editor_gui.py
@@ -4,3 +4,23 @@
 #after the process, there will be a button that gives the user an option to save the photo
 #files that will be supported will be jpg and jpeg files from the user. 
 #will strive to make the UI as modern as possible, while keeping it minimalistic
+
+import tkinter as tk
+import customtkinter
+
+#ui design
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("1920x1080")
+app.title("Film Photography Assistant")
+
+#adding ui elements
+
+title = customtkinter.CTkLabel
+
+
+#keep app running
+app.mainloop()
