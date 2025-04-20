0206bd5c1281a7ff98c539381e91b50d0ea9d570
nnathanyc <nathan31@illinois.edu>
2025-04-20 01:03:42 -0500

    gui development
    addition of title and upload image button

--- README.md
+++ README.md
@@ -3,13 +3,6 @@
 Final project for bcog200 - I plan to create an image gallery/portfolio that is interactive that houses my personal photos. I hope to be able to separate them into certain categories (urban, nature, personal). The application will strive to have a user-friendly and minimalistic design to make interactions as smooth as possible. 
 
 Functions
-
-1) Photogallery function (self) - This will be hte parent function that houses all of the layout features including category selections, dispaly size, resolution, and navigation buttons
-2) Load functions - there will be load functions for the given amount of photos I plan on loading.
-3) Display Function - Function will be used to display photos given the correct parameters within the portfolio
-
-or 
-
 1) Simplistic GUI where users can input photos
 2) the inputted photos will then be analyzed
 3) user taken to a screen where their image's camera settings are revealed
--- editor_gui.py
+++ editor_gui.py
@@ -6,20 +6,46 @@
 #will strive to make the UI as modern as possible, while keeping it minimalistic
 
 import tkinter as tk
+from tkinter import filedialog
 import customtkinter
+from PIL import Image, ImageTk
 
-#ui design
+
+#ui window
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
 app.geometry("1920x1080")
-app.title("Film Photography Assistant")
+app.title("Film Filter Application")
+
+
+#Function to upload image
+
+def upload_image():
+    file_path = filedialog.askopenfilename(filetypes=[("JPEG files", "*.jpg *.jpeg")], title="Select an Image")
 
 #adding ui elements
+title_font = customtkinter.CTkFont(family="Helvetica", size=20, weight="bold")
+title = customtkinter.CTkLabel(app, text='Upload Image', font=title_font)
+title.pack(padx=10, pady=10)
 
-title = customtkinter.CTkLabel
+upload_btn = customtkinter.CTkButton(app, text="Open Image", command=upload_image)
+upload_btn.pack(pady=10)
+
+
+#photo upload input (copy link first then transform it to upload function)
+
+link=customtkinter.CTkEntry(app, width=350, height=40)
+link.pack()
+
+
+
+
+
+
+
 
 
 #keep app running
